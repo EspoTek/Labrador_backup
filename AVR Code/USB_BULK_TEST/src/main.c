20654e25a34196a65b8dbdbf46cc2a8873e01072
EspoTek <admin@espotek.com>
2017-01-10 12:17:42 +1100

    Calibration test added
    and it looks promising!

--- AVR Code/USB_BULK_TEST/src/main.c
+++ AVR Code/USB_BULK_TEST/src/main.c
@@ -56,16 +56,17 @@ int main(void){
 	DFLLRC32M.COMP2 = 0xBB;
 	DFLLRC32M.COMP1= 0x80;  //0xBB80 = 48,000.  
 	DFLLRC32M.CTRL = 0x01; //Enable
-		
+	
+	//Turn on the 48MHz clock and scale it down to 24MHz
 	CCP = CCP_IOREG_gc;
 	CLK.PSCTRL = CLK_PSADIV_2_gc | CLK_PSBCDIV_1_1_gc;  //All peripheral clocks = CLKsys / 2.
 	//CLK.USBCTRL handled by udc
-	OSC.CTRL = OSC_RC32MEN_bm | OSC_RC2MEN_bm;  //Enable 2MHz, 32MHz references.
-	while(OSC.STATUS != (OSC_RC32MRDY_bm | OSC_RC2MRDY_bm)); //Wait for them all to be ready before continuing
-	
+	OSC.CTRL = OSC_RC32MEN_bm | OSC_RC2MEN_bm;  //Enable 32MHz reference.  Keep 2MHz on.
+	while(OSC.STATUS != (OSC_RC32MRDY_bm | OSC_RC2MRDY_bm)); //Wait for it to be ready before continuing		
+		
 	//4 step process from ASF manual.  Puts a 48MHz clock on the PLL output
 	OSC.CTRL |= OSC_RC2MEN_bm;  //1. Enable reference clock source.
-	OSC.PLLCTRL = OSC_PLLSRC_RC2M_gc | 25; //2. Set the multiplication factor and select the clock reference for the PLL.
+	OSC.PLLCTRL = OSC_PLLSRC_RC2M_gc | 24; //2. Set the multiplication factor and select the clock reference for the PLL.
 	while(!(OSC.STATUS & OSC_RC2MRDY_bm)); //3. Wait until the clock reference source is stable.
 	OSC.CTRL |= OSC_PLLEN_bm; //4. Enable the PLL
 	
@@ -73,6 +74,9 @@ int main(void){
 	while(!(OSC.STATUS & OSC_PLLRDY_bm));
 	CCP = CCP_IOREG_gc;
 	CLK.CTRL = CLK_SCLKSEL_PLL_gc;
+	
+	//Calibration test
+	DFLLRC2M.CALB -= 1;
 	
 	board_init();
 	udc_start();
